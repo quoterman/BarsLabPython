ac3a7e1217c394a17b3a85d2d0c0c31969256767
unknown <quoterman>
2014-02-13 08:49:29 +0400

    first

--- django20140210/task/project201402/project201402/settings.py
+++ django20140210/task/project201402/project201402/settings.py
@@ -121,9 +121,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-     'django.contrib.admin',
-	 'math2',
-	 'dog',
+    'django.contrib.admin',
+	'math2',
+	'dog',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
