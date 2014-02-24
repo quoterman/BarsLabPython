7844890aa02596eedf4478c203b14fb1a10c5692
quoterman <quoterman>
2014-02-24 16:15:09 +0400

    first

--- django20140219/task/project2/blog/urls.py
+++ django20140219/task/project2/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('blog.views',
     url(r'^$', 'index'),
+    url(r'^posts$', 'posts'),
     url(r'^posts/(?P<post_id>\d+)$', 'post'),
     url(r'^posts/new$', 'post_new'),
     url(r'^posts/(?P<post_id>\d+)/edit$', 'post_edit'),
--- django20140219/task/project2/blog/views.py
+++ django20140219/task/project2/blog/views.py
@@ -2,15 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from blog.models import Post
 
-
 def index(request):
+    return render(request,"Index.html")
+
+def posts(request):
     post = Post.objects.all()
-    return render(request,"Index.html", {"post" : post})
-    #return HttpResponse("INDEX.")
+    return render(request, "posts.html", {"post" : post})
 
 def post(request,post_id):
     post = Post.objects.get(id = post_id)
-    return render(request,"post.html", {"post" : post})
+    return render(request, "post.html", {"post" : post})
 
 def post_new(request):
     return render(request, "newPost.html")
