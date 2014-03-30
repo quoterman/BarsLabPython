045a895258219d750ba4a960f44457b3001f0368
ma <ma@fosslabs.ru>
2014-03-30 10:54:30 +0400

    'classwork from 26 mar, still without homework'

---
+++ django20140326/twitter_project/analytics/urls.py
@@ -0,0 +1,21 @@
+
+from django.conf.urls import url, patterns
+from django.views.generic import TemplateView, DetailView
+from twitter.models import Tweet
+from analytics.views import TweetListView, CurrentUserTweetListView, TweetInfoView
+
+urlpatterns = patterns('',
+    url(r'^$', TweetListView.as_view()),
+    url(r'^page(?P<page>\d+)$', TweetListView.as_view()),
+
+    url(r'^my/$', CurrentUserTweetListView.as_view()),
+    url(r'^my/page(?P<page>\d+)$', CurrentUserTweetListView.as_view()),
+
+    url(r'^home$', 
+    	TemplateView.as_view(template_name="analytics/dashboard.html")),
+
+    url(r'^tweetinfo/(?P<pk>\d+)$', TweetInfoView.as_view())
+
+    #url(r'^tweetinfo/(?P<pk>\d+)$', DetailView.as_view(model=Tweet,
+    #	template_name="analytics/tweetinfo.html"))
+)
